ddce133e4a6c1db1eddcb93a52951dd762636d88
Krishna Penukonda <penukonda.krishna.moorthy@gmail.com>
2020-04-02 01:48:16 +0800

    Make installable package

---
+++ setup.py
@@ -0,0 +1,46 @@
+import setuptools
+
+requirements = [
+    # Torch
+    "torchvision",
+    "pytorch-lightning",
+    "test-tube",
+    "torchsummary",
+    # Data handling
+    "numpy",
+    "Pillow",
+    # Visualization
+    "matplotlib",
+    "pydot",
+    "pydotplus",
+    # Misc
+    "IPython",
+    "joblib",
+    "natsort",
+    "tqdm",
+]
+
+dev_requirements = [
+    "jupyter",
+    "black"
+]
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="kitti_adapt",
+    version="0.0.1",
+    author="Krishna Penukonda",
+    url="",
+    long_description=long_description,
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: Other/Proprietary License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+    install_requires=requirements,
+    extras_require={"dev": dev_requirements,},
+)
