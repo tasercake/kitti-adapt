dea79b655ae504e2e25090bc374387d54caf0483
jenyangg <39770775+jenyangg@users.noreply.github.com>
2020-04-08 20:43:26 +0800

    Add files via upload

--- semanticKitti/RealData/vKitti.py
+++ semanticKitti/RealData/vKitti.py
@@ -1,6 +1,7 @@
 import xml.etree.ElementTree as ET
 import glob
 import cv2
+print(cv2.__version__)
 import torch
 from torchvision.datasets import MNIST
 from torchvision.transforms import transforms
@@ -15,7 +16,7 @@ from skimage import io, transform
 import torch.nn.functional as F
 from KittiDataloader import DataLoader_vkitti
 from KittiDataset import vKittiDataset
-
+import tqdm
 def run():
     # if torch.cuda.is_available():
     #     dev = "cuda:0"
@@ -28,21 +29,23 @@ def run():
     # if [0,199,255] in unique_arr:
     #     print('xxx')
     f = open('colors.txt','r')
-#    color_dic = {}
-#    num_class = 0
-#    for line in f:
-#        cat,r,g,b = line.split()
-#        color_dic[cat] = [r,g,b]
-#        num_class += 1
-#    print(num_class)
+    color_dic = {}
+    num_class = 0
+    for line in f:
+        cat,r,g,b = line.split()
+        color_dic[cat] = [r,g,b]
+        num_class += 1
+    print(num_class)
     print("Running kitti")
     batch_size = 2
+    selected_dims = (375, 1242, 3)
     transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize(mean=[0.485, 0.456, 0.406],
                                                                                 std=[0.229, 0.224, 0.225])])
+    # img_directory = "D:\\data_semantics\\training\\minitest"
     img_directory = "D:\\data_semantics\\training\\semantic_rgb"
 #    label_directory = "../data/VKitti_classSeg/Scene01/15-deg-left/frames/classSegmentation/Camera_0/test/"
-    model = models.segmentation.fcn_resnet50(pretrained=False, progress=True, num_classes=16, aux_loss=None)
-    kitti_dataset = vKittiDataset(img_directory,transform)
+    model = models.segmentation.fcn_resnet50(pretrained=False, progress=True, num_classes=num_class, aux_loss=None)
+    kitti_dataset = vKittiDataset(img_directory,color_dic,transform)
     dataloader = DataLoader_vkitti(kitti_dataset, model, batch_size)
     trainer = pl.Trainer(gpus=0)
     trainer.fit(dataloader)
