06e68002bb8f0307e06ebe32adb6dc93b4707a5a
IC088 <33095731+IC088@users.noreply.github.com>
2020-04-18 12:57:01 +0800

    Update main.py

--- v_kitti_detection/main.py
+++ v_kitti_detection/main.py
@@ -8,6 +8,8 @@ from loader.dataloader import KITTILoader
 import cv2
 import numpy as np
 import math
+from tqdm import tqdm
+from pprint import pprint
 
 
 import torch
@@ -16,7 +18,7 @@ import torch.optim as optim
 import torch.distributed as dist
 
 
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, random_split, ConcatDataset
 import os
 import torch
 from torchvision import transforms
@@ -25,6 +27,10 @@ import torchvision.models as models
 import torchvision
 
 from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
+
+import matplotlib.pyplot as plt
+
+from PIL import Image
 
 
 def is_dist_avail_and_initialized():
@@ -85,21 +91,8 @@ def move_to(obj, device):
 	else:
 		raise TypeError("Invalid type for move_to")
 
-def create_loader(image_path, target_path,labels_path, info_path, device):
-	'''
-	Input : 
-	- image_path : String ( path to the images )
-	- segmentation_path : ( path to masks )
-	Output: 
-	- dataloader : DataLoader () # To be Updated with train, val, test
-	'''
 
-	# preprocess = transforms.Compose([transforms.ToTensor(),transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),])
-	transform = transforms.Compose([transforms.ToTensor()])
-	dataset = KITTILoader(image_path, target_path,labels_path, info_path,device, transformation = transform)
-	# print(dataset[100])
-	dataloader = DataLoader(dataset, batch_size=1, shuffle= True)
-	return dataloader
+
 
 
 def train( model, device , train_loader , optimizer, epochs, loss_func ):
@@ -120,40 +113,58 @@ def train( model, device , train_loader , optimizer, epochs, loss_func ):
 
 	train_losses = []
 
-	correct = 0
+	train_loss = 0
 
-	for index, batch in enumerate(train_loader):
-
-		data = batch['images'].to(device)
-
-		targets = batch['target']
+	idx = 0 
 
 
-		targets = move_to(targets, device)
-
-		optimizer.zero_grad()
-
-		loss_dict = model(data, targets) #outputs losses instead of predicition
 
 
-		losses = sum(loss for loss in loss_dict.values())
+	with tqdm(total = len(train_loader)) as bar:
 
-		# reduce losses over all GPUs for logging purposes
-		loss_dict_reduced = reduce_dict(loss_dict)
-		losses_reduced = sum(loss for loss in loss_dict_reduced.values())
+		for index, batch in enumerate(train_loader):
 
-		loss_value = losses_reduced.item()
+			data = batch['images'].to(device)
 
-		optimizer.zero_grad()
-		losses.backward()
-		optimizer.step()
+			targets = batch['target']
 
-		train_loss = loss_value
+			targets = move_to(targets, device)
+
+			optimizer.zero_grad()
+
+			loss_dict = model(data, targets) #outputs losses instead of predicition
+
+			losses = sum(loss for loss in loss_dict.values())
+
+			# reduce losses over all GPUs for logging purposes
+			loss_dict_reduced = reduce_dict(loss_dict)
+			losses_reduced = sum(loss for loss in loss_dict_reduced.values())
+
+			loss_value = losses_reduced.item()
+
+			# print(loss_value)
+
+			optimizer.zero_grad()
+			losses.backward()
+			optimizer.step()
+
+			train_loss += loss_value
+
+			bar.update()
+
+			idx = index
+
+
+		train_loss /= (idx+1)
+		# print(idx + 1)
+	
+	print(f'\nEpoch {epochs} Train loss : {train_loss}')
+
 	return train_loss
 
 
 
-def validate ( model , device , val_loader , loss_func):
+def validate ( model , device , val_loader , loss_func, epochs):
 	'''
 	Function validate is used to check accuracy against the validation set
 	
@@ -167,61 +178,107 @@ def validate ( model , device , val_loader , loss_func):
 	- val_loss: float (validation loss)
 	- accuracy: float (validation accuracy)
 	'''
+	def match_size_calc_acc(t_target, t_pred):
+		if t_target.shape[0] > t_pred.shape[0]:
+			t_pred = t_pred.tolist()
+			for i in range(len(t_target) - len(t_pred)):
+				
+				t_pred.append(0)
+			t_pred = torch.tensor(t_pred)
+		elif t_target.shape[0] < t_pred.shape[0]:
+			t_target = t_target.tolist()
+			for i in range(len(t_pred) - len(t_target)):
+				t_target.append(0)
+			t_target = torch.tensor(t_target)
+
+		return t_pred, t_target
+
+
+
+	
 
 	model.eval().to(device)
 	val_loss = 0
-	accuracy = 0
+	val_accuracy = 0
 
+	val_dicts= []
 	print('Start Validation')
 
+	# with tqdm(total = len(val_loader)) as bar:
+
 	with torch.no_grad():
-		for _, batch in enumerate(val_loader):
-
+		for idx , batch in enumerate(val_loader):
 			data = batch['images'].to(device)
-
 			targets = batch['target']
-
-
 			targets = move_to(targets, device)
-			loss_dict = model(data, targets) #outputs losses instead of predicition
+			prediction = model(data, targets) #outputs prediction instead of losses in eval mode.
+			output = prediction[0]['scores']
+
+			mean_score = output.mean()
+
+			print(mean_score)
+
+			val_accuracy += mean_score
+
+				# bar.update()
+
+	mean_val_accuracy  = val_accuracy / len(val_loader)
+
+	print(f'Epoch {epochs} Validation Accuracy = {mean_val_accuracy}')
+	return mean_val_accuracy
+
+def image_scores(path, models, model_path, output_path, test_dataset, transformation, device = 'cuda'):
+	'''
+	Function is used to calculate the image prediction scores
+
+	Input :
+	- path : string (root directory for the dataset)
+	- models : model (model used in this task)
+	- model_path : string ( model directory )
+	- test_dataset : Dataset ( essentially the testing files )
+	- transformation : Transform ( Augmentation )
+	- device : string ('cuda')
+	'''
+
+	print(f'Loading model to score images. Scores saved {output_path}') # Testing the model
 
 
+	# model_file = torch.load(model_path)
+	# models.load_state_dict(model_file)
 
+	# models.to(device)
+	# models.eval()
+	# score_list = []
 
-		print('Validation: ')
-		print(loss_dict)
+	# with tqdm(total = len(test_dataset.images)) as bar:
+	# 	for name , labels in zip(test_dataset.images, test_dataset.labels):
+	# 		images = load_image(path, name, transformation)
+	# 		outputs,_ = models(images).max(0)
 
+	# 		outputs = torch.sigmoid(outputs)
 
+	# 		score_list.append((name, outputs))
+	# 		bar.update()
 
-	# 		data = batch['image'].to(device)
-	# 		target = batch['label'].long().to(device)
+	# pprint(score_list[:5])
 
-	# 		output = model(data)
-	# 		batch_loss = loss_func(output, target).item()#CrossEntropyLoss
-	# 		val_loss += batch_loss
+	# import pickle
 
-	# 		pred = output.argmax(dim=1, keepdim=True)
-	# 		accuracy += pred.eq(target.view_as(pred)).sum().item()
-	# val_loss /= len(val_loader)
+	# with open(output_path, 'wb') as f:
+	# 	pickle.dump(score_list,f)
 
-	# accuracy /= len(val_loader.dataset)
-
-	# print(f'Validation set: Average loss: {val_loss}, Accuracy: {accuracy}')
-	# return val_loss, accuracy
-
-
-def custom_training(train_loader, val_loader,device, epochs=5):
+def custom_training(train_loader, val_loader,device, epochs=3):
 	
 	model = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=True).to(device)
 
-	num_classes = 92
+	num_classes = 4
 
 	in_features = model.roi_heads.box_predictor.cls_score.in_features
 	# replace the pre-trained head with a new one
-	# model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
+	model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
 
-	optimizer = optim.SGD(model.parameters(), lr=0.01, momentum=0.9)
-	loss_func = nn.CrossEntropyLoss().to(device)
+	optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+	loss_func = nn.BCEWithLogitsLoss().to(device)
 
 
 	train_losses_vis = []
@@ -230,15 +287,107 @@ def custom_training(train_loader, val_loader,device, epochs=5):
 
 	print('Starting Training')
 
-	from pprint import pprint
-
 	for epoch in range(1, epochs + 1):
 		train_loss = train(model, device, train_loader, optimizer, epoch, loss_func)
-		print(train_loss)
-		validate(model, device, val_loader, loss_func)
-		
+		accuracy = validate(model, device, val_loader, loss_func, epoch)
+
+		if (len(train_losses_vis) > 0) and (train_loss < min(train_losses_vis)):
+			torch.save(model.state_dict(), 'object_detect_model.pt')
+
+		train_losses_vis.append(train_loss)
+
+		val_accuracy_list.append(accuracy)
+
+	return train_losses_vis, val_accuracy_list
 
 
+
+
+
+def get_labels_bbox(paths):
+	new_paths_info = []
+	new_paths_bbox = []
+	for path in paths:
+		info_path = os.path.join('labels', path, 'info.txt')
+		bbox_path = os.path.join('labels', path, 'bbox.txt')
+		new_paths_info.append(info_path)
+		new_paths_bbox.append(bbox_path)
+
+
+	return new_paths_info, new_paths_bbox
+
+
+def get_image(paths):
+	'''
+	Function to get the path lists for the images
+
+	INput:
+	- paths : list (path lists)
+	'''
+
+	camera_list = ['Camera_0']
+
+	new_list = []
+	new_path_list = []
+	for path in paths:
+		for camera in camera_list:
+			new_path = os.path.join(path, 'frames', 'rgb',camera)
+			new_list.append(new_path)
+
+	for new_path in new_list:
+		new_path_image = os.path.join('images', new_path)
+
+		new_path_list.append(new_path_image)
+
+
+	return new_path_list
+
+def create_dataset(image_paths, labels_path, info_paths, device):
+	'''
+	Function to create dataset from multiple sources
+
+	Inputs:
+	- image_paths : list of the image paths
+	- labels_path : list of the labels paths (in this case is the bounding boxes)
+	- 
+	'''
+
+	preprocess = transforms.Compose([transforms.ToTensor(),transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),])
+
+	datasetls = [] 
+	for image_path, label_path, info_path in zip(image_paths, labels_path, info_paths):
+
+		dataset = KITTILoader(image_path, label_path, info_path , device, transformation = preprocess)
+
+		datasetls.append(dataset)
+
+	all_dataset = ConcatDataset(datasetls)
+
+
+	return all_dataset
+
+def create_loader(dataset):
+	'''
+	function to create train and val loader
+
+	input :
+
+	- dataset : Dataset (dataset file that was already made)
+
+	Output:
+
+	- train_loader : DatasLoader (training loader)
+	- val_loader : Dataloader (validation loader)
+	'''
+
+	train_split = int(0.8*len(dataset))
+	val_split = len(dataset) - train_split
+
+
+	train_set, val_set = random_split(dataset,[train_split, val_split])
+	train_loader = DataLoader(train_set,batch_size=1, shuffle= True)
+	val_loader = DataLoader(val_set,batch_size=1, shuffle= True)
+	return train_loader, val_loader
 
 
 
@@ -246,35 +395,47 @@ def run():
 	
 	device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-	path = os.path.join('Scene01', '15-deg-left')
-	val_path = os.path.join('Scene01', '15-deg-right')
-
-	labels_path = os.path.join('labels', path)
-	segmentation = os.path.join('segmentation', path)
-
-	val_labels_path = os.path.join('labels', val_path)
-	val_segmentation = os.path.join('segmentation', val_path)
-
-	img_path = os.path.join('images', path, 'frames', 'rgb', 'Camera_0')
-
-	val_img_path = os.path.join('images', val_path, 'frames', 'rgb', 'Camera_0')
-
-	info_path = os.path.join(labels_path, 'info.txt')
-	labels_path = os.path.join(labels_path, 'bbox.txt')
-
-	val_info_path = os.path.join(val_labels_path, 'info.txt')
-	val_labels_path = os.path.join(val_labels_path, 'bbox.txt')
+	scene_lists = ['Scene01','Scene02'] # Change the umbrella folder name
 
 
-	segmentation_path = os.path.join(segmentation, 'frames', 'classSegmentation', 'Camera_0')
-	val_segmentation_path = os.path.join(val_segmentation, 'frames', 'classSegmentation', 'Camera_0')
+	# here change the sub folder name
+
+	types_list = ['15-deg-left', '15-deg-right', '30-deg-left', '30-deg-left', 'clone', 'fog', 'morning', 'overcast', 'rain', 'sunset']
+
+	# types_list =['15-deg-left']
+	paths = []
 
 
-	trainloader = create_loader(img_path, segmentation_path, labels_path, info_path,device)
+	for scene in scene_lists:
+		for types in types_list:
+			path = os.path.join(scene, types)
 
-	valloader = create_loader(val_img_path, val_segmentation_path, val_labels_path, val_info_path, device)
+			paths.append(path)
 
-	custom_training(trainloader,valloader, device)
+	info_paths, bbox_paths = get_labels_bbox(paths)
+	image_paths = get_image(paths)
+
+
+	all_dataset = create_dataset(image_paths, bbox_paths, info_paths, device)
+
+	print(len(all_dataset))
+
+	trainloader, valloader = create_loader(all_dataset)
+
+	torch.cuda.empty_cache()
+
+
+	if os.path.exists('object_detect_model.pt'):
+
+		print('Weights exists')
+	else:
+		print('weights not available. Starting training')
+		train_losses, val_accuracy = custom_training(trainloader,valloader, device)
+
+		print(train_losses, val_accuracy)
+
+
+
 
 
 if __name__ == '__main__':
