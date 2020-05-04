fd3de3a3e9fda55515340692eb433676c8615db4
teezhiyao <teezhiyao@gmail.com>
2020-05-04 17:51:18 +0800

    Update rename_files.py

--- semanticKitti/rename_files.py
+++ semanticKitti/rename_files.py
@@ -1,15 +1,13 @@
 # Pythono3 code to rename multiple
-# files in a directory or folder
-import glob
-# importing os module
 import os
 
-root_dir = r'C:\Users\teezh\Documents\GitHub\kitti-adapt\data\vKitti_RGB'
+root_dir = r'C:\Users\teezh\Documents\GitHub\kitti-adapt\data\vKitti_classSeg'
 len_root = len(root_dir)
 for subdir, dirs, files in os.walk(root_dir):
     for file in files:
         print(os.path.join(subdir[len_root:], file))
         print(file)
-        os.rename(os.path.join(subdir, file), os.path.join(subdir, file.replace('rgb', 'classgt').replace('.jpg','.png')))
+        # os.rename(os.path.join(subdir, file), os.path.join(subdir, file.replace('rgb', 'classgt').replace('.jpg','.png')))
+        os.rename(os.path.join(subdir, file), os.path.join(subdir, file.replace('classgt', 'rgb').replace('.png','.jpg')))
 
 
