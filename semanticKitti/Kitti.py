d78da1c66b085883574fa7c6da6f9e54ba540b31
teezhiyao <teezhiyao@gmail.com>
2020-05-04 17:22:47 +0800

    Update Kitti.py

--- semanticKitti/Kitti.py
+++ semanticKitti/Kitti.py
@@ -1,18 +1,7 @@
-import xml.etree.ElementTree as ET
-import glob
-import cv2
 import torch
-from torchvision.datasets import MNIST
 from torchvision.transforms import transforms
 import torchvision.models as models
-import numpy as np
-import matplotlib.pyplot as plt  # patch-wise similarities, droi images
 import pytorch_lightning as pl
-from torch.utils.data import DataLoader, random_split, Dataset, TensorDataset
-from torch.optim import Adam
-import os
-from skimage import io, transform
-import torch.nn.functional as F
 from KittiDataloader import DataLoader_kitti
 from KittiDataset import KittiDataset
 
@@ -40,6 +29,7 @@ def run():
     f = open('rColors.txt', 'r')
     real_color_dic = {}
     real_num_class = 0
+
     # desired_classes = ['Car', 'TrafficLight', 'TrafficSign', 'Pole', 'GuardRail', 'Vegetation', 'Terrain', 'Undefined', 'Sky', 'Road']
     desired_classes = ['Car', 'Undefined']
     for line in f:
@@ -56,10 +46,6 @@ def run():
     i = 0
     for child in model.children():
         i += 1
-        #         print(i)
-        #         print(child)
-        # for param in child.parameters():
-        #     print(param.requires_grad)
         print(i)
         if i < 3:
             print(i)
@@ -77,43 +63,11 @@ def run():
     real_kitti_dataset = KittiDataset(real_img_directory, real_label_directory, real_color_dic, transform)
 
     print('Creating Dataloader')
-    # All real
     dataloader1 = DataLoader_kitti(real_kitti_dataset, virtual_kitti_dataset, model, batch_size, 0, real_num_class,
                                    0.005)
-    # dataloader2 = DataLoader_kitti(real_kitti_dataset, virtual_kitti_dataset, model, batch_size, 0.25, real_num_class,
-    #                                0.005)
-    # dataloader3 = DataLoader_kitti(real_kitti_dataset, virtual_kitti_dataset, model, batch_size, 2, real_num_class,
-    #                                0.0005)
-    # dataloader4 = DataLoader_kitti(real_kitti_dataset, virtual_kitti_dataset, model, batch_size, 5, real_num_class,
-    #                                0.0005)
-    #     dataloader5 = DataLoader_kitti(real_kitti_dataset, virtual_kitti_dataset, model, batch_size,1,real_num_class,0.0005)
-
-    print('---------------------------------------------------------break 1 - 100% Real----------------------------')
     trainer = pl.Trainer(min_epochs=0, max_epochs=0, gpus=1, early_stop_callback=True)
     trainer.fit(dataloader1)
     trainer.test()
-    # print('---------------------------------------------------------break 2 - 75% Real----------------------------')
-    # trainer = pl.Trainer(min_epochs=5, max_epochs=10, gpus=1, early_stop_callback=True)
-    # trainer.fit(dataloader2)
-    # trainer.test()
-    # print('---------------------------------------------------------break 3 - 50% Real----------------------------')
-    # trainer = pl.Trainer(min_epochs=5, max_epochs=10, gpus=1, early_stop_callback=True)
-    # trainer.fit(dataloader3)
-    # trainer.test()
-    # print('---------------------------------------------------------break 4 - 25% Real----------------------------')
-    # trainer = pl.Trainer(min_epochs=5, max_epochs=10, gpus=1, early_stop_callback=True)
-    # trainer.fit(dataloader4)
-    # trainer.test()
-    # #     print('---------------------------------------------------------break 5 - 0% Real----------------------------')
-    # #     trainer = pl.Trainer(min_epochs=5,max_epochs=10,gpus=1,early_stop_callback=True)
-    # #     trainer.fit(dataloader5)
-    # #     trainer.test()
-    #
-    # #     trainer = pl.Trainer(min_epochs=5,max_epochs=5,gpus=1)
-    # #     trainer.fit(dataloader6)
-    # #     trainer.test()
-    # #     print('---------------------------------------------------------break 1 - 50% Real  50% Virtual----------------------------')
-
     print('I am done')
 
 
