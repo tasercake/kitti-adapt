3ed0142b927a23a6f0484ca95c742e59ebb1767a
teezhiyao <teezhiyao@gmail.com>
2020-05-04 17:22:44 +0800

    Update rename_files.py

--- semanticKitti/rename_files.py
+++ semanticKitti/rename_files.py
@@ -10,8 +10,6 @@ for subdir, dirs, files in os.walk(root_dir):
     for file in files:
         print(os.path.join(subdir[len_root:], file))
         print(file)
-        # print(os.path.join(subdir, file.replace('rgb', 'classgt').replace('.jpg','.png')))
-        # os.rename(os.path.join(subdir, file),
-        #           os.path.join(subdir, file.replace('rgb', 'classgt').replace('.jpg','.png')))
+        os.rename(os.path.join(subdir, file), os.path.join(subdir, file.replace('rgb', 'classgt').replace('.jpg','.png')))
 
 
