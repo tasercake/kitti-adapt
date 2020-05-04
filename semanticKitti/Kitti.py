97513ece97ede9d33cb9af88632a41ddc9fc86c2
BillioJeverson <billio_jeverson@mymail.sutd.edu.sg>
2020-05-05 03:19:46 +0800

    Revert "Revert "Merge branch 'master' into VirtualKITTI-detection""
    This reverts commit cc5f43eabbb71f78715c77eee865fd469cf2a2e5.

---
+++ semanticKitti/Kitti.py
@@ -0,0 +1,78 @@
+import torch
+from torchvision.transforms import transforms
+import torchvision.models as models
+import pytorch_lightning as pl
+from KittiDataloader import DataLoader_kitti
+from KittiDataset import KittiDataset
+
+
+def run():
+    if torch.cuda.is_available():
+        dev = "cuda:0"
+    else:
+        dev = "cpu"
+    device = torch.device(dev)
+
+    # Parameters
+    # Desired_classes = ['Car', 'TrafficLight', 'TrafficSign', 'Pole', 'GuardRail', 'Vegetation', 'Terrain', 'Undefined', 'Sky', 'Road']
+    desired_classes = ['Car', 'Undefined']
+    virtual_percentage = 0
+    batch_size = 1
+    num_epochs = 0
+    lr = 0.005
+
+    ## Processing vColors.txt which contains the rgb values for the segmented image
+    f = open('vColors.txt', 'r')
+    virtual_color_dic = {}
+    virtual_num_class = 0
+
+    for line in f:
+        cat, r, g, b = line.split()
+        if cat in desired_classes:
+            virtual_color_dic[cat] = [r, g, b]
+            virtual_num_class += 1
+
+    ### Processing rColors_org.txt which contains the rgb values for the segmented image
+    f = open('rColors.txt', 'r')
+    real_color_dic = {}
+    real_num_class = 0
+    for line in f:
+        cat, r, g, b = line.split()
+        if cat in desired_classes:
+            real_color_dic[cat] = [r, g, b]
+            real_num_class += 1
+
+    transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                                                                std=[0.229, 0.224, 0.225])])
+    print('Number of classes', real_num_class)
+    model = models.segmentation.fcn_resnet101(pretrained=True, progress=True, aux_loss=None)
+    i = 0
+    for child in model.children():
+        i += 1
+        print(i)
+        if i < 3:
+            print(i)
+            for param in child.parameters():
+                param.requires_grad = False
+
+    print('Reading Virtual Data')
+    vir_img_directory = '../data/vKitti_RGB',
+    vir_label_directory = '../data/vKitti_classSeg'
+    virtual_kitti_dataset = KittiDataset(vir_img_directory, vir_label_directory, virtual_color_dic, transform)
+    print(len(virtual_kitti_dataset))
+    print('Reading Real Data')
+    real_img_directory = "../data/data_semantics/training/image_2"
+    real_label_directory = "../data/data_semantics/training/semantic_rgb"
+    real_kitti_dataset = KittiDataset(real_img_directory, real_label_directory, real_color_dic, transform)
+
+    print('Creating Dataloader')
+    dataloader1 = DataLoader_kitti(real_kitti_dataset, virtual_kitti_dataset, model, batch_size, virtual_percentage, real_num_class,
+                                   lr)
+    trainer = pl.Trainer(min_epochs=num_epochs, max_epochs=num_epochs, gpus=1, early_stop_callback=True)
+    trainer.fit(dataloader1)
+    trainer.test()
+    print('I am done')
+
+
+if __name__ == '__main__':
+    run()
---
+++ semanticKitti/KittiDataloader.py
@@ -0,0 +1,225 @@
+import glob
+import torch
+from torchvision.transforms import transforms
+import pytorch_lightning as pl
+from torch.utils.data import DataLoader, random_split, Dataset, TensorDataset
+from torch.optim import Adam
+import numpy as np, os, sys
+import torch.nn as nn
+
+
+class DataLoader_kitti(pl.LightningModule):
+    def __init__(self, rdataset, vdataset, model, batch_size, virtual_data_ratio, num_class, lr):
+        super(DataLoader_kitti, self).__init__()
+        self.lr = lr
+        self.vdataset = vdataset
+        self.rdataset = rdataset
+        self.batch_size = batch_size
+
+        ### Ratio for splitting data into the different sets
+        self.virtual_ratio = virtual_data_ratio
+        self.rdataset_length = len(rdataset)
+        self.vdataset_length = len(vdataset)
+
+        ### 60% of data used for testing, 20% for validation and 20% for testing (For 100% real data),
+        # with synthetic data, amount of data used for testing remains the same while ratio for training and validation set changes.
+        # rtrain_sampler, rval_sampler, rtest_sampler = self.split_data_3set(self.rdataset, 0.6,0.8)
+        if virtual_data_ratio > 1:
+            desired_r_train_size = np.int(self.rdataset_length * 0.5)
+
+        else:
+            desired_r_train_size = np.int(self.rdataset_length * 0.5 * (1 - virtual_data_ratio))
+
+        desired_r_val_size = np.int(self.rdataset_length * 0.25)
+        desired_r_test_size = np.int(self.rdataset_length * 0.25)
+        desired_v_train_size = np.int((self.rdataset_length * 0.5) * virtual_data_ratio)
+        desired_v_val_size = np.int((self.rdataset_length * 0.25) * virtual_data_ratio)
+        self.rtrain_indices, self.rval_indices, self.rtest_indices = self.split_data_return_indices_3(
+            self.rdataset_length, desired_r_train_size, desired_r_val_size, desired_r_test_size)
+
+        self.vtrain_indices, self.vval_indices = self.split_data_return_indices_2(self.vdataset_length,
+                                                                                  desired_v_train_size,
+                                                                                  desired_v_val_size)
+        self.train_loader = 0
+        self.val_loader = 0
+        self.test_loader = 0
+
+        ### Needed as our model used is for image segmentation
+        self.feature_extractor = model
+        self.feature_extractor.eval()
+        self.conv = nn.Conv2d(21, num_class, kernel_size=(1, 1), stride=(1, 1))
+
+    def forward(self, x):
+        x = self.feature_extractor(x)['out']
+        # print(x)
+        x = self.conv(x)
+        return x
+
+    def split_data_3set(self, dataset, split_ratio1, split_ratio2, seed=1337):
+        dataset_size = len(dataset)
+        indices = list(range(dataset_size))
+        split_1 = int(np.floor(split_ratio1 * dataset_size))
+        split_2 = int(np.floor(split_ratio2 * dataset_size))
+        if 1:
+            np.random.seed(seed)
+            np.random.shuffle(indices)
+        set1_indices, set2_indices, set3_indices = indices[:split_1], indices[split_1:split_2], indices[split_2:]
+        set1_sample = torch.utils.data.sampler.SubsetRandomSampler(set1_indices)
+        set2_sample = torch.utils.data.sampler.SubsetRandomSampler(set2_indices)
+        set3_indices = torch.utils.data.sampler.SubsetRandomSampler(set3_indices)
+
+        return set1_sample, set2_sample, set3_indices
+
+    def split_data_return_indices_2(self, dataset_size, sample_size1, sample_size2, seed=1337):
+        indices = list(range(dataset_size))
+        sample2_end_index = sample_size1 + sample_size2
+        if 1:
+            np.random.seed(seed)
+            np.random.shuffle(indices)
+        set1_indices, set2_indices = indices[:sample_size1], indices[sample_size1:sample2_end_index]
+        # set1_sample = torch.utils.data.sampler.SubsetRandomSampler(set1_indices)
+        # set2_sample = torch.utils.data.sampler.SubsetRandomSampler(set2_indices)
+        return set1_indices, set2_indices
+
+    def split_data_return_indices_3(self, dataset_size, sample_size1, sample_size2, sample_size3, seed=1337):
+        indices = list(range(dataset_size))
+        sample2_end_index = sample_size1 + sample_size2
+        sample3_end_index = sample2_end_index + sample_size3
+        if 1:
+            np.random.seed(seed)
+            np.random.shuffle(indices)
+        set1_indices, set2_indices, set3_indices = indices[:sample_size1], indices[
+                                                                           sample_size1:sample2_end_index], indices[
+                                                                                                            sample2_end_index:sample3_end_index]
+        # set1_sample = torch.utils.data.sampler.SubsetRandomSampler(set1_indices)
+        # set2_sample = torch.utils.data.sampler.SubsetRandomSampler(set2_indices)
+        # set3_sample = torch.utils.data.sampler.SubsetRandomSampler(set3_indices)
+        return set1_indices, set2_indices, set3_indices
+
+    def split_data(self, dataset, split_ratio1, seed=1337):
+        dataset_size = len(dataset)
+        indices = list(range(dataset_size))
+        split_1 = int(np.floor(split_ratio1 * dataset_size))
+        if 1:
+            np.random.seed(seed)
+            np.random.shuffle(indices)
+        set1_indices, set2_indices = indices[:split_1], indices[split_1:]
+        set1_sample = torch.utils.data.sampler.SubsetRandomSampler(set1_indices)
+        set2_sample = torch.utils.data.sampler.SubsetRandomSampler(set2_indices)
+
+        return set1_sample, set2_sample
+
+    def prepare_data(self):
+        vtrain_dataset = torch.utils.data.Subset(self.vdataset, self.vtrain_indices)
+        vval_dataset = torch.utils.data.Subset(self.vdataset, self.vval_indices)
+
+        rtrain_dataset = torch.utils.data.Subset(self.vdataset, self.rtrain_indices)
+        rval_dataset = torch.utils.data.Subset(self.vdataset, self.rval_indices)
+        rtest_dataset = torch.utils.data.Subset(self.vdataset, self.rtest_indices)
+        print('vtrain len: %d vval len: %d rtrain len: %d rval len: %d rtest len: %d' % (len(vtrain_dataset),
+                                                                                         len(vval_dataset),
+                                                                                         len(rtrain_dataset),
+                                                                                         len(rval_dataset),
+                                                                                         len(rtest_dataset)))
+        training_dataset = torch.utils.data.ConcatDataset([vtrain_dataset, rtrain_dataset])
+        validation_dataset = torch.utils.data.ConcatDataset([vval_dataset, rval_dataset])
+        self.train_loader = DataLoader(training_dataset, batch_size=self.batch_size, shuffle=True)
+        self.val_loader = DataLoader(rval_dataset, batch_size=self.batch_size, shuffle=True)
+        self.test_loader = DataLoader(rtest_dataset)
+
+    def train_dataloader(self):
+        return self.train_loader
+
+    def val_dataloader(self):
+        return self.val_loader
+
+    def test_dataloader(self):
+        return self.test_loader
+
+    def configure_optimizers(self):
+        return Adam(self.parameters(), lr=self.lr)
+
+    def training_step(self, batch, batch_idx):
+        x, label = batch
+        output = self.forward(x)
+        criterion = torch.nn.BCEWithLogitsLoss(reduction='sum')
+        loss = criterion(output, label)
+        #         print('output_pred data: ', output_pred.data)
+        #         print('label data: ', label_max.data)
+        #         if batch_idx % 20 == 0:
+        #             print(str(batch_idx) + 'Loss ' + str(loss))
+        return {'loss': loss}
+
+    def validation_step(self, batch, batch_idx):
+        ### (batch_size, number of classes, spatial dim)
+        x, label = batch
+        output = self.forward(x)
+        criterion = torch.nn.BCEWithLogitsLoss(reduction='sum')
+        loss = criterion(output, label)
+        output_pred = output.argmax(1)
+        label_max = label.argmax(1)
+        #         print(label.dtype)
+        label = label.cpu().type(torch.LongTensor).numpy()
+        max_ind = torch.argmax(output, 1, keepdim=True).cpu()
+        one_hot = torch.FloatTensor(output.shape)
+        one_hot.zero_()
+        one_hot.scatter_(1, max_ind, 1)
+        one_hot = one_hot.type(torch.LongTensor).numpy()
+        #         print('one hot', one_hot)
+        #         print('label', label)
+        running_corrects = np.sum((output_pred.data == label_max.data).cpu().numpy())
+        running_total = label_max.shape[0] * label_max.shape[1] * label_max.shape[2]
+        #         iou_intersect = (one_hot & label).float().sum((1, 2,3))
+        #         iou_union = (one_hot | label).float().sum((1,2,3))
+        iou_intersect = np.sum(np.logical_and(one_hot, label))
+        iou_union = np.sum(np.logical_or(one_hot, label))
+        #         print('iou intersect', iou_intersect)
+        #         print('iou iou_union', iou_union)
+        return {'val_loss': loss, 'running_correct': running_corrects, 'running_total': running_total,
+                'iou_intersect': iou_intersect, 'iou_union': iou_union}
+
+    def validation_epoch_end(self, outputs):
+        SMOOTH = 1e-6
+        val_loss_mean = torch.stack([x['val_loss'] for x in outputs]).mean()
+        val_corr = np.sum([x['running_correct'] for x in outputs])
+        val_total = np.sum([x['running_total'] for x in outputs])
+        val_acc = float(val_corr) / float(val_total)
+        val_intersect = np.sum([x['iou_intersect'] for x in outputs])
+        val_union = np.sum([x['iou_union'] for x in outputs])
+        print('IOU', (val_intersect + SMOOTH) / (val_union + SMOOTH))
+        print('validation accuracy', val_acc)
+        return {'val_loss': val_loss_mean, 'accuracy': val_acc}
+
+    def test_step(self, batch, batch_idx):
+        x, label = batch
+        output = self.forward(x)
+        criterion = torch.nn.BCEWithLogitsLoss(reduction='sum')
+        loss = criterion(output, label)
+        output_pred = output.argmax(1)
+        label_max = label.argmax(1)
+        running_corrects = np.sum((output_pred.data == label_max.data).cpu().numpy())
+        running_total = label_max.shape[0] * label_max.shape[1] * label_max.shape[2]
+
+        label = label.cpu().type(torch.LongTensor)
+        max_ind = torch.argmax(output, 1, keepdim=True).cpu()
+        one_hot = torch.FloatTensor(output.shape)
+        one_hot.zero_()
+        one_hot.scatter_(1, max_ind, 1)
+        one_hot = one_hot.type(torch.LongTensor)
+
+        iou_intersect = (one_hot & label).float().sum((1, 2, 3))
+        iou_union = (one_hot | label).float().sum((1, 2, 3))
+        return {'test_loss': loss, 'running_correct': running_corrects, 'running_total': running_total,
+                'iou_intersect': iou_intersect, 'iou_union': iou_union}
+
+    def test_epoch_end(self, outputs):
+        SMOOTH = 1e-6
+        test_loss_mean = torch.stack([x['test_loss'] for x in outputs]).mean()
+        test_corr = np.sum([x['running_correct'] for x in outputs])
+        test_total = np.sum([x['running_total'] for x in outputs])
+        test_acc = float(test_corr) / float(test_total)
+        test_intersect = np.sum([x['iou_intersect'] for x in outputs])
+        test_union = np.sum([x['iou_union'] for x in outputs])
+        print('IOU', (test_intersect + SMOOTH) / (test_union + SMOOTH))
+        print('test accuracy', test_acc)
+        return {'test_loss': test_loss_mean, 'accuracy': test_acc}
